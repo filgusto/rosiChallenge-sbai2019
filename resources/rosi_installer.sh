a2925494db57bba8db3f95e87e2661aae0b7a3d4
Filipe Rocha <filipe.rocha@ufrj.br>
2019-06-27 18:04:21 -0300

    improved installation script

--- resources/rosi_installer.sh
+++ resources/rosi_installer.sh
@@ -12,29 +12,41 @@ NC='\033[0m' # No Color
 
 # messages to the user
 echo -e "${GREEN}=========================================${NC}"
-echo -e "${BLUE}** ROSI CHALLENGE SIMULATOR INSTALLER **${NC}\n"
+echo -e "${GREEN}=== ROSI CHALLENGE SIMULATOR INSTALLER ==${NC}"
+echo -e "${GREEN}=========================================${NC}\n"
 
+# reads and treats the caktin_ws location
 echo -e "${BLUE}First, tell me where is located your catkin_ws folder (example: /home/john/catkin_ws):${NC}"
 read var_catkin_ws_folder
 
-echo -e "\n${BLUE}Do you use bash or zsh? (answer: bash, zsh)${NC}"
+# checks if this directory exists
+if [ ! -d $var_catkin_ws_folder ]; then
+	echo -e "${RED}It appears that this path does not exists. Stopping now this script.${NC}"
+	exit 1
+fi
+
+#reads and treats the zsh existence
+echo -e "\n${BLUE}Do you have zsh? (answer: y, n)${NC}"
 read var_shell
 
 # treats the rc file
-if [[ $var_shell == "zsh" ]]; then
+if [[ $var_shell == "y" ]]; then
 	editorc=".zshrc"
-elif [[ $var_shell == "bash" ]]; then
+elif [[ $var_shell == "n" ]]; then
 	editorc=".bashrc"
+else 
+	echo -e "${RED}This input is not valid. Stopping now this script.${NC}"
+	exit 1
 fi
 
 # install some support packages
-echo -e "${BLUE}\nInstalling some support packages\n${ORANGE}We need your sudo password here:${NC}"
-sudo apt update 
-sudo apt install -f python-catkin-tools xsltproc ros-$ROS_DISTRO-brics-actuator ros-$ROS_DISTRO-tf2-sensor-msgs ros-$ROS_DISTRO-joy 
+echo -e "${BLUE}\nInstalling some support packages\n${ORANGE}(We might need your sudo password here)${NC}"
+#sudo apt update #UNCOMMENT
+#sudo apt install -f python-catkin-tools xsltproc ros-$ROS_DISTRO-brics-actuator ros-$ROS_DISTRO-tf2-sensor-msgs ros-$ROS_DISTRO-joy #UNCOMMENT
 
 # downloads V-REP
 echo -e "\n${BLUE}\nDownloading V-REP from Coppelia Robotics server.${NC}"
-wget http://coppeliarobotics.com/files/V-REP_PRO_EDU_V3_6_2_Ubuntu18_04.tar.xz 
+#wget http://coppeliarobotics.com/files/V-REP_PRO_EDU_V3_6_2_Ubuntu18_04.tar.xz #UNCOMMENT
 
 # extract, move and delete V-REP folder
 echo -e "\n${BLUE}Extracting V-REP to $vrep_folder.${NC}"
@@ -42,26 +54,31 @@ tar -xf ./V-REP_PRO_EDU_V3_6_2_Ubuntu18_04.tar.xz
 mv ./V-REP_PRO_EDU_V3_6_2_Ubuntu18_04 $vrep_folder
 rm -rf ./V-REP_PRO_EDU_V3_6_2_Ubuntu18_04.tar.xz
 
-# insert some stuff into bashrc or zshrc
-echo -e "\n${BLUE}Adding some lines to $HOME/$editorc.${NC}"
+# insert some stuff into bashrc
+echo -e "\n${BLUE}Adding some lines to $HOME/.bashrc${NC}"
+echo -e "\n\n\n#ROSI SIMULATION additions " >> $HOME/.bashrc
+echo "export ROS_CATKIN_WS='$var_catkin_ws_folder'" >> $HOME/.bashrc
+echo "export VREP_ROOT='$vrep_folder'" >> $HOME/.bashrc
+echo "alias vrep='$vrep_folder/vrep.sh'" >> $HOME/.bashrc
 
-echo -e "\n\n\n#ROSI SIMULATION additions " >> $HOME/$editorc
-echo "export ROS_CATKIN_WS='$var_catkin_ws_folder'" >> $HOME/$editorc
-echo "export VREP_ROOT='$vrep_folder'" >> $HOME/$editorc
-echo "alias vrep='$vrep_folder/vrep.sh'" >> $HOME/$editorc
-
-echo -e "\n\n${RED} $ROS_CATKIN_WS \n\n${NC}$"
+# adds also to zshrc, if the user have it
+if [[ $var_shell == "y" ]]; then
+	echo -e "\n${BLUE}Adding some lines to $HOME/.zshrc ${NC}"
+	echo -e "\n\n\n#ROSI SIMULATION additions " >> $HOME/.zshrc
+	echo "export ROS_CATKIN_WS='$var_catkin_ws_folder'" >> $HOME/.zshrc
+	echo "export VREP_ROOT='$vrep_folder'" >> $HOME/.zshrc
+	echo "alias vrep='$vrep_folder/vrep.sh'" >> $HOME/.zshrc
+fi
 
 #sourcing again bashrc
-echo -e "\n${BLUE}Sourcing $HOME/$editorc.${NC}"
-source $HOME/$editorc
+source $HOME/.bashrc
 
 #compiles rosi_defy package into catkin_ws
 echo -e "\n${BLUE}Compiling rosi_defy package to generate its messages.\nWe have to first clean your catkin_ws environment\n${ORANGE}Select 'YES' if asked to remove catkin_ws directories.${NC}"
 cd $ROS_CATKIN_WS
 catkin clean 
 catkin build
-source $HOME/$editorc
+source $HOME/.bashrc
 
 #clones the vrep_ros_interface
 echo -e "\n${BLUE}Cloning the V-REP ROS Interface into $ROS_CATKIN_WS/src/vrep_ros_interface.${NC}"
@@ -77,22 +94,23 @@ sed -i '27i\    rosi_defy' $ROS_CATKIN_WS/src/vrep_ros_interface/CMakeLists.txt
 echo -e "\n${BLUE}Re-compiling the ROS workspace.${NC}"
 cd $ROS_CATKIN_WS
 catkin build
-source $HOME/$editorc
+source $HOME/.bashrc
 
 # copying the vrep library to vrep_root
 echo -e "\n${BLUE}Copying vrep_ros_interface library to VREP_ROOT.${NC}"
 cp $ROS_CATKIN_WS/devel/lib/libv_repExtRosInterface.so $vrep_folder
 
 # Copy ROSI model to vrep models folder
-echo -e "$\n{BLUE}Copying ROSI model to V-REP robot models"
+echo -e "\n${BLUE}Copying ROSI model to V-REP robot models"
 cp $ROS_CATKIN_WS/src/rosi_defy/vrep_content/rosi_model.ttm $vrep_folder/models/robots/mobile/ROSI.ttm
 
 #sourcing again bashrc
-echo -e "\n${BLUE}Sourcing $HOME/$editorc.${NC}"
-source $HOME/$editorc
+source $HOME/.bashrc
 
 # final messages 
-echo -e "${GREEN}\n\nFinished installing and configuring V-REP and ROS interface."
+echo -e "${GREEN}\n\nFinished installing and configuring V-REP and ROS interface!${NC}"
+echo -e "${GREEN}\nIf you are using zsh, please source it again.${NC}"
 echo -e "${GREEN}Now you can run V-REP in terminal using the command ${ORANGE}vrep${BLUE}.${NC}"
-echo -e "${GREEN}Check if everything is OK since this code does not check its actions.${NC}"
+echo -e "${GREEN}Check if everything is OK, since this code does not check its actions.${NC}"
+echo -e "\n${BLUE}HAVE FUN! :).${NC}"
 echo -e "${GREEN}=========================================${NC}"
