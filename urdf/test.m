6c68b4d4b6577e39b4690561e813b790da02b934
Filipe Rocha <f.rocha41@gmail.com>
2019-08-27 13:46:16 -0300

    added velodyne and kinect to the urdf

--- urdf/test.m
+++ urdf/test.m
@@ -2,7 +2,9 @@ clear all;
 close all;
 
 % loading rosi urdf
-rosi = importrobot('rosi.urdf');
-
+%
 % displaying rosi
-show(rosi);
+% show(rosi);
+
+t_s_v = [-3.300e-2 -1.000e-3 8.585e-2].';
+t_b_s = [2.750e-1 -9.997e-4 7.968e-2].';
